9bbb8096c0e48867f36df8c4583e82e5571caa8a
mmorenoco <mmcorrales@outlook.com>
2021-09-14 17:39:34 +0200

    fallos mapas

--- apiusersinterface/src/views/Maps.vue
+++ apiusersinterface/src/views/Maps.vue
@@ -1,13 +1,49 @@
 <template>
-    <div class="map"></div>
+    <div id="map"></div>
 </template>
 
 <script>
-export default {
+import { Loader } from "@googlemaps/js-api-loader"
 
+export default {
+    name: 'Maps',
+    methods: {
+        loadMap() {
+
+            const loader = new Loader({
+                apiKey: "AIzaSyBmKeMZYfBGqIKwbiSy4t-OQfGs-f1otcA",
+                version: "weekly",
+                libraries: ["places"]
+            })
+
+            const mapOptions = {
+                center: {
+                    lat: -34.397,
+                    lng: 150.644
+                },
+                zoom: 4
+            }
+
+            loader
+            .load()
+            .then((google) => {
+                new google.maps.Map(document.getElementById("map"), mapOptions);
+                this.$nextTick()
+                this.$forceUpdate()
+            })
+            .catch(e => {
+                console.log(e)
+            });
+        }
+    },
+    async mounted() {
+         await this.loadMap()
+    }
 }
 </script>
 
 <style>
-
+    #map {
+        height: 100%;
+    }
 </style>
